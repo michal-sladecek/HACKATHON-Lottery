b3a40941ebc8e7697ade01efed4fb34f2cb256e3
USER <USER@PROBOOK470>
2016-05-19 21:39:05 +0200

    Initial

---
+++ Loteria/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\user\documents\visual studio 2015\Projects\Loteria\Loteria\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
